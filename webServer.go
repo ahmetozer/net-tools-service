e3f21387b56f3399c35f342f8d2a7a11dc22d793
Ahmet <10574489+AhmetOZER@users.noreply.github.com>
2020-05-21 18:09:13 +0300

    isMayIPv4 and isMayIPv6 funtions defined. It checks function and given host is same type.

--- webServer.go
+++ webServer.go
@@ -8,7 +8,7 @@ import (
 	"os/exec"
 	"regexp"
 	//	"os"
-	"time"
+
 	//"math/rand"
 	//"encoding/base64"
 	"strings"
@@ -18,7 +18,7 @@ import (
 
 // pass CMD output to HTTP
 func writeCmdOutput(res http.ResponseWriter, pipeReader *io.PipeReader) {
-	var BUFLEN = 1024 // for
+	var BUFLEN = 4096 // for
 	buffer := make([]byte, BUFLEN)
 	for {
 		n, err := pipeReader.Read(buffer)
@@ -31,12 +31,33 @@ func writeCmdOutput(res http.ResponseWriter, pipeReader *io.PipeReader) {
 		res.Write(data)
 		if f, ok := res.(http.Flusher); ok {
 			f.Flush()
+			fmt.Printf(fmt.Sprint(ok))
+
 		}
 		//reset buffer
 		for i := 0; i < n; i++ {
 			buffer[i] = 0
 		}
 	}
+}
+
+var (
+	/*
+		Regexs for checking input
+	*/
+	ipv6Regex   = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`
+	ipv4Regex   = `(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`
+	domainRegex = `(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$`
+)
+
+func isMayIPv4(host string) bool {
+	match, _ := regexp.MatchString(ipv4Regex+`|`+domainRegex, host)
+	return match
+}
+
+func isMayIPv6(host string) bool {
+	match, _ := regexp.MatchString(ipv6Regex+`|`+domainRegex, host)
+	return match
 }
 
 func webServer(logger *log.Logger) *http.Server {
@@ -46,14 +67,17 @@ func webServer(logger *log.Logger) *http.Server {
 
 	// Index Handler
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "text/html; charset=utf-8")
 		w.WriteHeader(http.StatusNotFound)
-
-		fmt.Fprintf(w, "Error page %s not found.", r.URL)
+		fmt.Fprintf(w, `<title="Looking Glass Server"/><h2>Error page `+fmt.Sprint(r.URL)+
+			` not found.</h2></br><p>Looking Glass Server. For more information, visit <a href="https://ahmetozer.org/">ahmetozer.org</a></p>`)
 	})
 
 	// Get System Disk informations with linux util lsblk . // JSON
 	router.HandleFunc("/system-status.php", func(w http.ResponseWriter, r *http.Request) {
-
+		/*
+			Get arguments from url
+		*/
 		functype := r.URL.Query().Get("type")
 		host := r.URL.Query().Get("host")
 		nameserver := r.URL.Query().Get("nameserver")
@@ -62,13 +86,10 @@ func webServer(logger *log.Logger) *http.Server {
 		mobile := r.URL.Query().Get("mobile")
 		term := r.URL.Query().Get("term")
 		setLiveOutputHeaders(w)
-		/*
-			Regexs for checking input
-		*/
-		ipv6Regex := `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`
-		ipv4Regex := `(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`
-		domainRegex := `(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$`
 
+		/*
+			Check host input
+		*/
 		match, _ := regexp.MatchString(ipv4Regex+`|`+ipv6Regex+`|`+domainRegex, host)
 		if !match {
 			w.WriteHeader(http.StatusBadRequest)
@@ -85,9 +106,17 @@ func webServer(logger *log.Logger) *http.Server {
 			//cmd.Dir = "/empty/"
 			args := []string{"-l 3", "-c 5", "-i 0.3", "-s 64", "-t 64", "-W 1", "-q"}
 			if functype == "icmp4" {
+				if !isMayIPv4(host) {
+					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatch\"}")
+					return
+				}
 				args = append(args, "-4")
 			}
 			if functype == "icmp6" {
+				if !isMayIPv6(host) {
+					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatch\"}")
+					return
+				}
 				args = append(args, "-6")
 			}
 			args = append(args, host)
@@ -95,16 +124,18 @@ func webServer(logger *log.Logger) *http.Server {
 			//err := cmd.Run()
 			out, err := cmd.CombinedOutput()
 
-			if err != nil {
-				// If error occur on ping command.
+			if err != nil { // If error occur on ping command.
+				// If given input type wich is IPv4 or IPv6 and run type is not match this error will be occur
 				if fmt.Sprint(err) == "exit status 2" {
-					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatch\"}")
+					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatchExecuted\"}")
 					return
 				}
+				//	When the ping command cannot access the server, this error will be occur
 				if fmt.Sprint(err) == "exit status 1" {
 					fmt.Fprintf(w, "{\"code\":\"RemoteHostDown\"}")
 					return
 				}
+				// If Any un expected occur, this will be shown
 				fmt.Fprintf(w, "{\"code\":\"UnknownExit\",\"exitCode\":\""+fmt.Sprint(err)+"\",\"execOut:\""+string(out)+"\"}")
 
 			} else {
@@ -116,25 +147,15 @@ func webServer(logger *log.Logger) *http.Server {
 				mdevLoc := strings.Index(outString, "/mdev =")
 				rttOut := outString[mdevLoc+8 : mdevLoc+strings.Index(outString[mdevLoc+1:], "ms")]
 				// parse rtt status
-				rttOutParsed := strings.Split(rttOut, "/")
-
+				rttOutParsed := strings.Split(rttOut, "/") // [0] rtt min , [1] avg, [2] max, [3] mdev
+				// Get other data from program output.
 				transmittedPacketCount := outString[strings.Index(outString, "ping statistics ---")+20 : strings.Index(outString, " packets transmitted,")]
 				recivedPacketCount := outString[strings.Index(outString, " packets transmitted,")+22 : strings.Index(outString, " received,")]
 				packetLoss := outString[strings.Index(outString, " received,")+11 : strings.Index(outString, " packet loss,")]
 
-				strings.IndexAny(outString, "---")
-				strings.Index(outString, "ping statistics ---")
-				// [0] rtt min , [1] avg, [2] max, [3] mdev
-
 				//fmt.Fprint(w)
 				//fmt.Fprint(w, rttOutParsed[0]+"\n"+transmittedPacketCount+"\n"+recivedPacketCount+"\n"+packetLoss)
-				type FruitBasket struct {
-					Name    string
-					Fruit   []string
-					Id      int64  `json:"ref"`
-					private string // An unexported field is not encoded.
-					Created time.Time
-				}
+
 				fmt.Fprint(w, `{"code":"OK", "rttmin":"`+rttOutParsed[0]+`", "rttavg":"`+rttOutParsed[1]+`", "rttmax":"`+rttOutParsed[2]+`", "mdev":"`+
 					rttOutParsed[3]+`", "packetloss":"`+packetLoss+`", "recivedPacketCount": "`+recivedPacketCount+`", "transmittedPacketCount":"`+transmittedPacketCount+`","functype":"`+functype+`"}`)
 
@@ -145,8 +166,56 @@ func webServer(logger *log.Logger) *http.Server {
 		case "webkontrol":
 		case "time":
 		case "whois":
-		case "nslookup":
+			args := []string{host}
+			if term == "" {
+				w.Header().Set("content-type", "text/html; charset=utf-8")
+				fmt.Fprintf(w, "<style>body {background-color: #2d3436}</style><pre style='white-space: pre-line;  font-size: 16px; font-family: Arial, Helvetica, sans-serif;  color: #dfe6e9'>")
+			}
+			cmd := exec.Command("whois", args...)
+			// Organize pipelines
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				// If error occur on ping command.
+				if fmt.Sprint(err) == "exit status 1" {
+					fmt.Fprintf(w, string(out))
+					return
+				}
+				fmt.Fprintf(w, "{\"code\":\"UnknownExit\",\"exitCode\":\""+fmt.Sprint(err)+"\",\"execOut:\""+string(out)+"\"}")
 
+			} else {
+				//  Execute output to convert string
+				fmt.Fprintf(w, string(out))
+			}
+			return
+		case "nslookup":
+			args := []string{host}
+			if nameserver != "" {
+				match, _ := regexp.MatchString(ipv4Regex+`|`+ipv6Regex+`|`+domainRegex, nameserver)
+				if !match {
+					w.WriteHeader(http.StatusBadRequest)
+					fmt.Fprintf(w, "ERR: Host is not IPv4, IPv6 or domain")
+					return
+				}
+				args = append(args, nameserver)
+			}
+			if term == "" {
+				w.Header().Set("content-type", "text/html; charset=utf-8")
+				fmt.Fprintf(w, "<style>body {background-color: #2d3436}</style><pre style='white-space: pre-line;  font-size: 16px; font-family: Arial, Helvetica, sans-serif;  color: #dfe6e9'>")
+			}
+			cmd := exec.Command("nslookup", args...)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				// If error occur on command.
+				if fmt.Sprint(err) == "exit status 1" {
+					fmt.Fprintf(w, string(out))
+					return
+				}
+				fmt.Fprintf(w, "{\"code\":\"UnknownExit\",\"exitCode\":\""+fmt.Sprint(err)+"\",\"execOut:\""+string(out)+"\"}")
+
+			} else {
+				//  Execute output to convert string
+				fmt.Fprintf(w, string(out))
+			}
 			/*************************
 			Live output for ping frame
 			**************************/
@@ -154,23 +223,31 @@ func webServer(logger *log.Logger) *http.Server {
 
 			args := []string{"-c 10", "-i 0.2"}
 
-			if mobile == "1" {
+			if mobile == "1" { // No resolve domain names to reduce widht of ping output to shown in mobile in better
 				args = append(args, "-n")
 			}
 			if functype == "ping4" {
+				if !isMayIPv4(host) {
+					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatch\"}")
+					return
+				}
 				args = append(args, "-4")
 			}
 			if functype == "ping6" {
+				if !isMayIPv6(host) {
+					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatch\"}")
+					return
+				}
 				args = append(args, "-6")
 			}
 
-			// If requests comes from iframe , not term
+			// If requests comes from iframe (not term), add style to iframe
 			if term == "" {
 				w.Header().Set("content-type", "text/html; charset=utf-8")
 				fmt.Fprintf(w, "<style>body {background-color: #2d3436}</style><pre style='white-space: pre-line;  font-size: 16px; font-family: Arial, Helvetica, sans-serif;  color: #dfe6e9'>")
 			}
 
-			args = append(args, host)
+			args = append(args, host) // add host to arguments
 
 			cmd := exec.Command("ping", args...)
 			// Organize pipelines
@@ -196,8 +273,16 @@ func webServer(logger *log.Logger) *http.Server {
 			}
 
 			if functype == "tracert4" {
+				if !isMayIPv4(host) {
+					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatch\"}")
+					return
+				}
 				args = append(args, "-4")
 			} else if functype == "tracert6" {
+				if !isMayIPv6(host) {
+					fmt.Fprintf(w, "{\"code\":\"FuncTypeMissMatch\"}")
+					return
+				}
 				args = append(args, "-6")
 			}
 
@@ -205,7 +290,7 @@ func webServer(logger *log.Logger) *http.Server {
 				w.Header().Set("content-type", "text/html; charset=utf-8")
 				fmt.Fprintf(w, "<style>body {background-color: #2d3436}</style><pre style='white-space: pre-line;  font-size: 16px; font-family: Arial, Helvetica, sans-serif;  color: #dfe6e9'>")
 			}
-			args = append(args, host)
+			args = append(args, host) // add host to arguments
 
 			cmd := exec.Command("traceroute", args...)
 			// Organize pipelines
@@ -213,32 +298,33 @@ func webServer(logger *log.Logger) *http.Server {
 			cmd.Stdout = pipeWriter
 			cmd.Stderr = pipeWriter
 			// Pass to web output
-			go writeCmdOutput(w, pipeIn)
+			go writeCmdOutput(w, pipeIn) // live output
 			// Run command
 			cmd.Run()
 			pipeWriter.Close()
-
+			return
 		case "curlkontrol":
 		case "curltamkontrol":
 		case "curldurum":
 		default:
+			// if any unknown function name given.
 			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintf(w, "404")
-			fmt.Fprintf(w, functype+host+nameserver+port+id+mobile+term)
+			fmt.Fprintf(w, "404 \n"+functype+host+nameserver+port+id+mobile+term)
 			return
 		}
 
 	})
 
+	// return as a webServer
 	return &http.Server{
 
 		Addr:     *listenAddr,
 		Handler:  router,
 		ErrorLog: logger,
 		/* Close sockets */
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  15 * time.Second,
+		//ReadTimeout:  5 * time.Second,
+		//WriteTimeout: 10 * time.Second,
+		//IdleTimeout:  15 * time.Second,
 	}
 }
 
