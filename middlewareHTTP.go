decae69e9cc4aa9c10fa65a81e172b06036bd372
Ahmet <10574489+AhmetOZER@users.noreply.github.com>
2020-07-25 02:52:53 +0300

    referrer control update and comments added to middleware.

--- middlewareHTTP.go
+++ middlewareHTTP.go
@@ -7,36 +7,50 @@ import (
 	"net/url"
 )
 
+// This is SECURITY Control Middleware
 func middlewareHTTPHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get Client IP address for Rate Limitter.
 		ip, _, err := net.SplitHostPort(r.RemoteAddr) // _ is port but not required.
+
+		// If system cannot parse the addr. (may be socket in next)
 		if err != nil {
-			fmt.Fprintf(w, "userip: %q is not IP:port", r.RemoteAddr)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, `{"code":"%s","err":"%s"}`, http.StatusText(http.StatusInternalServerError), err.Error())
+			return
 		}
 
-		var httpScheme string
+		// Define request method.
+		httpScheme := "https"
 		if r.TLS == nil {
 			httpScheme = "http"
-		} else {
-			httpScheme = "https"
 		}
+
+		// This may be requests comes from different domain, port or http scheme.
 		if httpScheme+"://"+r.Host != serverConfig["ThisServerURL"] {
 			w.WriteHeader(http.StatusNotAcceptable)
-			fmt.Fprintf(w, `{"code":"NotAcceptable","err":Request Domain is different", "requestDomain": "%s"}`, httpScheme+"://"+r.Host)
+			fmt.Fprintf(w, `{"code":"NotAcceptable","err":"Request Domain is different", "requestDomain": "%s"}`, httpScheme+"://"+r.Host)
 			return
 		}
+
+		// Referer Control for forbiding embedding this service to unknown websites.
 		middleReferer := r.Header.Get("referer")
 
+		// Parse referer url to get host.
 		u, err := url.Parse(middleReferer)
 		if err != nil {
-			http.Error(w, http.StatusText(http.StatusInternalServerError)+err.Error(), http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, `{"code":"%s","err":"%s"}`, http.StatusText(http.StatusInternalServerError), err.Error())
 			return
 		}
+		// Check is referer in list.
+		if !contains(allowedReferers, u.Host) {
+			w.WriteHeader(http.StatusNotAcceptable)
+			fmt.Fprintf(w, `{"code":"NotAcceptable","err":"Referrer is not allowed", "referrer": "%s"}`, u.Host)
+			return
+		}
+		// If every basic security checks is ok, Let's control the ratio of request from client to preventing over usage and also any proxy.
 
-		if contains(allowedReferers, u.Host) {
-			http.Error(w, http.StatusText(http.StatusNotAcceptable), http.StatusNotAcceptable)
-			return
-		}
 		limiter := limiter.GetLimiter(ip)
 		if !limiter.Allow() {
 			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
