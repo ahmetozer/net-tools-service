15de22b8e0a0d7bfedad1632cf3ccb02fbd86c75
Ahmet OZER <git@ahmetozer.org>
2020-08-28 15:37:12 +0300

    Get data from ini

---
+++ functions.go
@@ -0,0 +1,17 @@
+package main
+
+import "strings"
+
+func getINI(iniText string, key string) string {
+
+	keyLoc := strings.Index(iniText, key)
+	if keyLoc == -1 {
+		return ""
+	}
+	newLineLoc := strings.Index(iniText[keyLoc:], "\n")
+	if newLineLoc == -1 {
+		return ""
+	}
+	keyLocAdjusted := keyLoc + len(key)
+	return iniText[keyLocAdjusted+1 : keyLoc+newLineLoc]
+}
